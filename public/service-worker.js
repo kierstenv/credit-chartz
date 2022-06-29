2ca3fc81623fc9ad5d238eba77e116749a22a311
kierstenv <kierstenvicknair22@gmail.com>
2022-06-29 03:17:59 -0500

    fix service worker

--- public/service-worker.js
+++ public/service-worker.js
@@ -3,10 +3,10 @@ const VERSION = "version_01";
 const CACHE_NAME = APP_PREFIX + VERSION;
 
 const FILES_TO_CACHE = [
-  "index.html",
-  "js/index.js",
-  "js/idb.js",
-  "css/styles.css"
+  "./index.html",
+  "./js/index.js",
+  "./js/idb.js",
+  "./css/styles.css"
 ];
 
 self.addEventListener('fetch', function(e) {
