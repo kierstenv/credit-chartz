dbc402ec7c54f7411c4d805667c3ec20c9a2aca0
kierstenv <kierstenvicknair22@gmail.com>
2022-06-28 22:40:43 -0500

    fix service worker

--- public/service-worker.js
+++ public/service-worker.js
@@ -3,10 +3,10 @@ const VERSION = "version_01";
 const CACHE_NAME = APP_PREFIX + VERSION;
 
 const FILES_TO_CACHE = [
-  "index.html",
-  "css/style.css",
-  "js/idb.js",
-  "js/index.js",
+  "./index.html",
+  "./css/style.css",
+  "./js/idb.js",
+  "./js/index.js",
 ];
 
 self.addEventListener('install', function (e) {
