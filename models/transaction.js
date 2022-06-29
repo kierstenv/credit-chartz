e704ba4523053aafc360fa4389e55665ca5a6272
kierstenv <kierstenvicknair22@gmail.com>
2022-06-28 19:30:53 -0500

    add web manifest

--- models/transaction.js
+++ models/transaction.js
@@ -7,11 +7,11 @@ const transactionSchema = new Schema(
     name: {
       type: String,
       trim: true,
-      required: "Enter a name for transaction"
+      required: [true, "Please enter a transaction name."]
     },
     value: {
       type: Number,
-      required: "Enter an amount"
+      required: [true, "Please enter a transaction amount."]
     },
     date: {
       type: Date,
