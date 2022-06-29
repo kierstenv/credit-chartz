53e0241af878d6d4d50b2a195bc8651c81a3aaf1
kierstenv <kierstenvicknair22@gmail.com>
2022-06-28 23:27:10 -0500

    fix service worker

--- public/service-worker.js
+++ public/service-worker.js
@@ -9,38 +9,35 @@ const FILES_TO_CACHE = [
   "./js/index.js",
 ];
 
-self.addEventListener('fetch', function(e) {
-  console.log('fetch request : ' + e.request.url)
-  e.respondWith(
-    caches.match(e.request).then(function (request) {
-      return request || fetch(e.request)
-  })
-  )
-})
-
 self.addEventListener('install', function(e) {
   e.waitUntil(
-    caches.open(CACHE_NAME).then(function (cache) {
+    caches.open(CACHE_NAME).then(function(cache) {
       console.log('installing cache : ' + CACHE_NAME);
       return cache.addAll(FILES_TO_CACHE);
     })
-  );
-});
-
+    );
+  });
+  
 self.addEventListener('activate', function(e) {
   e.waitUntil(
-    caches.keys().then(function (keyList) {
-      let cacheKeeplist = keyList.filter(function (key) {
-        return key.indexOf(APP_PREFIX);
-      });
-      cacheKeeplist.push(CACHE_NAME);
-
-      return Promise.all(keyList.map(function (key, i) {
-        if (cacheKeeplist.indexOf(key) === -1) {
-          console.log('deleting cache : ' + keyList[i] );
-          return caches.delete(keyList[i]);
-        }
-      })
-      );
+    caches.keys().then(function(keyList) {
+    let cacheKeeplist = keyList.filter(function(key) {
+      return key.indexOf(APP_PREFIX);
+    });
+    cacheKeeplist.push(CACHE_NAME);
+    return Promise.all(keyList.map(function(key, i) {
+      if (cacheKeeplist.indexOf(key) === -1) {
+        console.log('deleting cache : ' + keyList[i] );
+        return caches.delete(keyList[i]);
+      }
     }));
+  }));
+});
+
+self.addEventListener('fetch', function(e) {
+  console.log('fetch request : ' + e.request.url)
+  e.respondWith(
+    caches.match(e.request).then(function(request) {
+      return request || fetch(e.request);
+  }))
 });
