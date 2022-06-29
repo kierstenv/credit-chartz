63d5bdb55cf79d49a9f7dc95336a12066ee02328
kierstenv <kierstenvicknair22@gmail.com>
2022-06-28 20:04:53 -0500

    activate service worker

--- public/service-worker.js
+++ public/service-worker.js
@@ -17,3 +17,36 @@ self.addEventListener('install', function (e) {
     })
   );
 });
+
+self.addEventListener('activate', function (e) {
+  e.waitUntil(
+    caches.keys().then(function (keyList) {
+      let cacheKeeplist = keyList.filter(function (key) {
+        return key.indexOf(APP_PREFIX);
+      });
+      cacheKeeplist.push(CACHE_NAME);
+
+      return Promise.all(keyList.map(function (key, i) {
+        if (cacheKeeplist.indexOf(key) === -1) {
+          console.log('deleting cache : ' + keyList[i] );
+          return caches.delete(keyList[i]);
+        }
+      })
+      );
+    }));
+});
+
+self.addEventListener('fetch', function (e) {
+  console.log('fetch request : ' + e.request.url)
+  e.respondWith(
+    caches.match(e.request).then(function (request) {
+      if (request) {
+        console.log('responding with cache : ' + e.request.url)
+        return request
+      } else {
+        console.log('file is not cached, fetching : ' + e.request.url)
+        return fetch(e.request)
+    }
+  })
+  )
+})
